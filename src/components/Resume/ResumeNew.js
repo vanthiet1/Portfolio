6a2b251631f5a877ea84ee4a0e6971f61b7beb4f
van Thiết <tretrauthiet@gmail.com>
2025-02-22 17:00:10 +0700

    update cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/cv-thiet.pdf";
+import pdf from "../../Assets/Nguyen_Van_Thiet_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
